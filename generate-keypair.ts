9b46eae5194208befd605b2375979153bfae7281
Anish Gajbhare <anishgajbhare@Anishs-MacBook-Air.local>
2024-08-12 05:26:38 +0530

    generated keypairs

---
+++ generate-keypair.ts
@@ -0,0 +1,16 @@
+import { Keypair } from "@solana/web3.js"
+
+const keypair = Keypair.generate();
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`Finished!`);
+
+// Loading an existing keypair from an .env file
+
+// import "dotenv/config";
+// import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+// const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+// console.log(`Finished! We1have loaded our secret key securely, using an env file!`);
